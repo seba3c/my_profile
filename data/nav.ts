9777a7d083eb009ea3f954263025cf5575d4fbd0
Sebastian Castañeda <castanedacs@gmail.com>
2026-08-27 12:45:50 -0300

    Removed some sections to have an abbreviated site version

--- data/nav.ts
+++ data/nav.ts
@@ -1,9 +1,9 @@
 export const navLinks = [
   { href: '#about', label: 'About' },
   { href: '#tech-stack', label: 'My tech stack' },
-  { href: '#featured-projects', label: 'Featured Projects' },
-  { href: '#featured-events', label: 'Events' },
+  //{ href: '#featured-projects', label: 'Featured Projects' },
+  //{ href: '#featured-events', label: 'Events' },
   { href: '#experiences', label: 'Experiences' },
   { href: '#certifications', label: 'Certifications' },
-  { href: '#academic', label: 'Academic' },
+  //{ href: '#academic', label: 'Academic' },
 ] as const;
